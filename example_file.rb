fc28649d08c41d5c90bf41025c8e4c1ce874b5f7
Lawrence Jackson <l.jackson96@outlook.com>
2020-03-01 16:58:42 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,3 @@
+puts "You know what year it is??"
+this_year = 2020
+puts "Hey, it's not #{this_year} !" unless this_year == 2020
---
+++ learning-comments.rb
@@ -0,0 +1,5 @@
+#name = "Byron"
+name = "lisa"
+
+puts "We're sorry, but per health rules, #{name} is 
+not allowed in the store."
